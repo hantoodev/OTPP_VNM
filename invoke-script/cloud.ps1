3ec03b667d174f379a7e042f23b280daf2369841
Namm <namb20994@gmail.com>
2025-05-22 19:56:57 +0700

    Delete invoke-script/cloud.ps1

--- invoke-script/cloud.ps1
+++
@@ -1,5 +0,0 @@
-$batUrl = "https://raw.githubusercontent.com/NammIsADev/OptimizedToolsPlusPlus/main-development/invoke-script/stable.bat"
-$batPath = "$env:TEMP\stable.bat"
-
-Invoke-WebRequest -Uri $batUrl -OutFile $batPath
-Start-Process -FilePath $batPath -Wait
