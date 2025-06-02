af6cec5ff080c73c84afea25cbd4a1f028862f03
Namm <namb20994@gmail.com>
2025-06-02 16:56:48 +0700

    more UI

--- invoke-script/cloud.ps1
+++ invoke-script/cloud.ps1
@@ -1,9 +1,15 @@
+Write-Host "=========================================" -ForegroundColor Cyan
+Write-Host "         Please wait, preparing...   " -ForegroundColor Yellow
+Write-Host "=========================================" -ForegroundColor Cyan
+
 # Define URL and output path
 $batUrl = "https://raw.githubusercontent.com/NammIsADev/OptimizedToolsPlusPlus/main-development/invoke-script/stable.bat"
 $batPath = Join-Path -Path (Get-Location) -ChildPath "stable.bat"
 
-# Download the .bat file
+Write-Host "`nDownloading essential files..." -ForegroundColor Green
 Invoke-WebRequest -Uri $batUrl -OutFile $batPath -UseBasicParsing
+
+Write-Host "Download complete. Launching tool..." -ForegroundColor Green
 
 # Execute the .bat file
 Start-Process -FilePath $batPath -Wait
